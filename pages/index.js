f55e66fe27f20511c0fbc36b3ca662898d8277b1
sartkthegr8 <49450634+sartkthegr8@users.noreply.github.com>
2023-12-13 21:10:57 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,11 +5,18 @@ export default function Home() {
     const [items, setItems] = useState([]);
     const [message, setMessage] = useState('');
 
-    
+    const handleAddItem=useCallback(()=>{
+       const newItemName=`Item ${items.length + 1}`
+       setItems((prevItems)=>[...prevItems, newItemName])
+       setMessage('Item added successfully!')
+       setTimeout(()=>{
+           setMessage('')
+       },3000)
+    },[items])
 
     return (
         <div>
-            <button >Add Item</button>
+            <button onClick={handleAddItem}>Add Item</button>
             <ItemList items={items} />
             {message && <p id='message'>{message}</p>}
         </div>
